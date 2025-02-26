665421d02925a6d3b1fe4a2ad1a2bc5c79a62aef
ajkfds <ajkfds2018@gmail.com>
2025-02-27 06:41:43 +0900

    initial commit

---
+++ ajkCefGlue/Views/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+
+namespace ajkCefGlue.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBox_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    {
+        if(e.Key == Avalonia.Input.Key.Enter)
+        {
+            string? address = addressTextBox.Text;
+            if (address == null) return;
+            browser.Address = address;
+        }
+    }
+
+}
